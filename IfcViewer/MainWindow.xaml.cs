d84b65673eabbbfa2423c966b407c11601e0ac4a
Oualid R. <oualid.rhechim@gmail.com>
2025-03-26 12:03:09 +0100

    render a little triangle on screen

--- IfcViewer/MainWindow.xaml.cs
+++ IfcViewer/MainWindow.xaml.cs
@@ -1,23 +1,41 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Media3D;
+using HelixToolkit.Wpf;
 
-namespace IfcViewer;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace IfcViewer
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        //InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Build a simple triangle manually
+            var builder = new MeshBuilder();
+            builder.AddTriangle(
+                new Point3D(0, 0, 0),
+                new Point3D(1, 0, 0),
+                new Point3D(0, 1, 0)
+            );
+
+            var mesh = builder.ToMesh();
+
+            // Create a red material
+            var material = new DiffuseMaterial(new SolidColorBrush(Colors.Red));
+
+            // Build model and assign back material
+            var model = new GeometryModel3D(mesh, material)
+            {
+                BackMaterial = material
+            };
+
+            // Wrap into ModelVisual3D
+            var visual = new ModelVisual3D { Content = model };
+            viewport.Children.Add(visual);
+
+            // Adjust camera to show the mesh
+            viewport.ZoomExtents();
+        }
     }
 }
